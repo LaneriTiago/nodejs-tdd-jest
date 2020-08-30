c6cd6ba4674148b338d2f78e0d0d579fc16e0460
Tiago Luis Laneri <tiago.laneri93@gmail.com>
2020-08-30 18:34:24 -0300

    tdd and jest project

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'root',
+  password: 'kvw%xtm?!4279596',
+  database: 'usuarios',
+  dialect: 'mysql',
+  define: {
+    timestamps: true,
+    underscored: true, //aplicação de nome com underscore e não camelcase
+    underscoredAll: true, //aplicação de nome com underscore e não camelcase
+  },
+}
